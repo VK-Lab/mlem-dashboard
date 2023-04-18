a7bd2556e56755d061043ea118baae1ccbbcfc75
Liam <hoailinh1210@gmail.com>
2023-04-18 17:48:33 +0700

    refactor: remove nft button

--- src/components/EmptyData/index.tsx
+++ src/components/EmptyData/index.tsx
@@ -1,14 +1,14 @@
-import { Link, Button, ButtonProps, Typography, Box } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Image from 'next/image'; // No wrapper
 
 import ImgNoData from '@/assets/images/img--no-data.webp';
 
-interface ButtonLinkProps extends ButtonProps {
-  component: React.ComponentType;
-  target: string;
-  rel: string;
-}
+// interface ButtonLinkProps extends ButtonProps {
+//   component: React.ComponentType;
+//   target: string;
+//   rel: string;
+// }
 
 const StyledImage = styled(Image)(() => ({
   maxWidth: 720,
@@ -22,20 +22,20 @@ const StyledImage = styled(Image)(() => ({
   top: -30,
 }));
 
-const StyledButtonLink = styled((props: ButtonLinkProps) => (
-  <Button {...props} />
-))(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-  color: theme.palette.primary.contrastText,
-  boxShadow: '7px 8px 18px 0px rgb(0 0 0 / 10%)',
-  fontSize: theme.typography.pxToRem(16),
-  fontWeight: 600,
-  padding: '12px 24px',
-  marginLeft: theme.spacing(1),
-  '&:hover': {
-    borderColor: theme.palette.primary.main,
-  },
-}));
+// const StyledButtonLink = styled((props: ButtonLinkProps) => (
+//   <Button {...props} />
+// ))(({ theme }) => ({
+//   backgroundColor: theme.palette.primary.main,
+//   color: theme.palette.primary.contrastText,
+//   boxShadow: '7px 8px 18px 0px rgb(0 0 0 / 10%)',
+//   fontSize: theme.typography.pxToRem(16),
+//   fontWeight: 600,
+//   padding: '12px 24px',
+//   marginLeft: theme.spacing(1),
+//   '&:hover': {
+//     borderColor: theme.palette.primary.main,
+//   },
+// }));
 
 const StyledText = styled(Typography)(({ theme }) => ({
   fontSize: theme.typography.pxToRem(18),
@@ -50,15 +50,14 @@ const EmptyData = () => {
     <Box sx={{ maxWidth: '100%', textAlign: 'center' }}>
       <StyledText variant="h5">
         You do not have any Melem NFTs yet
-        <StyledButtonLink
+        {/* <StyledButtonLink
           variant="contained"
           component={Link}
           target="_blank"
-          href="https://testnets.opensea.io/collection/d2e-cards-v2"
           rel="noopener noreferrer"
         >
           Buy your first NFT
-        </StyledButtonLink>
+        </StyledButtonLink> */}
       </StyledText>
       <StyledImage src={ImgNoData} alt="Empty data" />
     </Box>
