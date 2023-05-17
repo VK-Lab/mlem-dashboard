703acc212730e57d11063d101b8bba6f3bd89bbb
Liam <hoailinh1210@gmail.com>
2023-05-17 16:33:44 +0700

    refactor: remove unused package

--- src/modules/AdminNft/ButtonBulkCreateModal/BulkNftForm/index.tsx
+++ src/modules/AdminNft/ButtonBulkCreateModal/BulkNftForm/index.tsx
@@ -1,12 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import { LoadingButton } from '@mui/lab';
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
-import { ethers, providers } from 'ethers';
-import _get from 'lodash/get';
-import _isString from 'lodash/isString';
 import {
   AutocompleteElement,
   FormContainer,
@@ -17,20 +14,13 @@ import { useQueryClient } from 'react-query';
 import * as yup from 'yup';
 
 import { StyledTextFieldElement } from './styled';
-import { NFT_COLLECTION_ABI } from '@/abis/nft-collection';
 import ToastMessage from '@/components/Toast';
 import { QueryKeys } from '@/enums/queryKeys.enum';
 import { useMutateBatchCreateNfts } from '@/hooks/mutations';
 import { useGetBenefits, useGetAllNftCollections } from '@/hooks/queries';
-import { useI18nToast } from '@/hooks/useToast';
-import {
-  BatchCreateNftsParams,
-  CreateNftParams,
-} from '@/services/admin/nft/types';
+import { CreateNftParams } from '@/services/admin/nft/types';
 import { Benefit } from '@/types/benefit';
 import { NftCollection } from '@/types/nft-collection';
-import { batchMintNfts } from '@/utils/ethers';
-import { generateMetadataUrl } from '@/utils/metadata';
 
 const validationSchema = yup.object({
   total: yup
@@ -49,31 +39,14 @@ type Props = {
 };
 
 const BulkNftForm = ({ onSuccess }: Props) => {
-  const { toastError } = useI18nToast();
   const queryClient = useQueryClient();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(false);
+  const [isLoading] = useState(false);
+  const [isDisabled] = useState(false);
   const {
     data: { items = [] } = { items: [], total: 0 },
     isLoading: isLoadingBenefits,
   } = useGetBenefits();
   const batchCreateNftsMutation = useMutateBatchCreateNfts({
-    onMutate: async ({ nfts }: BatchCreateNftsParams) => {
-      try {
-        const tokenAddress = _get(nfts, '[0].tokenAddress', '');
-        const walletAddress = _get(window.ethereum, 'selectedAddress', '');
-
-        const uris = nfts.map((nft: CreateNftParams) => {
-          return generateMetadataUrl(nft.tokenAddress, nft.tokenId);
-        });
-
-        await batchMintNfts(walletAddress, tokenAddress, uris);
-      } catch (err) {
-        toastError(_get(err, 'code', ''));
-
-        throw err;
-      }
-    },
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: [QueryKeys.LIST_NFTS] });
       ToastMessage({
@@ -124,48 +97,6 @@ const BulkNftForm = ({ onSuccess }: Props) => {
     data: { items: nftCollections } = { items: [], total: 0 },
     isLoading: isLoadingCollections,
   } = useGetAllNftCollections();
-
-  useEffect(() => {
-    const subcriber = formContext.watch(async (value, { name }) => {
-      if (name === 'tokenAddress') {
-        setIsLoading(true);
-        setIsDisabled(false);
-        if (!value.tokenAddress) {
-          return;
-        }
-        if (!window.ethereum) {
-          return;
-        }
-        const provider = new ethers.providers.Web3Provider(
-          window.ethereum as providers.ExternalProvider
-        );
-        const contract = new ethers.Contract(
-          value.tokenAddress,
-          NFT_COLLECTION_ABI,
-          provider
-        );
-
-        const tokenId = await contract.nextTokenIdToMint();
-        const owner = await contract.owner();
-
-        const selectedAddress = _get(window.ethereum, 'selectedAddress', '');
-
-        if (
-          !_isString(owner) ||
-          owner.toLowerCase() !== selectedAddress.toLowerCase()
-        ) {
-          setIsDisabled(true);
-        }
-
-        formContext.setValue('tokenId', tokenId.toString());
-        setIsLoading(false);
-      }
-    });
-
-    return () => {
-      subcriber.unsubscribe();
-    };
-  }, [formContext]);
 
   return (
     <FormContainer formContext={formContext} onSuccess={handleOnSubmitForm}>
