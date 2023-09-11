ec2746addc3131073350a9e532704c2fa7f7b59d
liam <hoailinh1210@gmail.com>
2023-09-11 21:28:16 +0700

    chore: remove token address on test

--- src/utils/metadata.test.ts
+++ src/utils/metadata.test.ts
@@ -7,7 +7,6 @@ vitest.mock('url-join');
 
 describe('generateMetadataUrl', () => {
   it('should generate the correct metadata URL', () => {
-    const tokenAddress = '0x123';
     const tokenId = '456';
 
     // Mock the urlJoin function to return a fixed URL
@@ -16,13 +15,12 @@ describe('generateMetadataUrl', () => {
     const expectedUrl = 'https://example.com/metadata';
 
     // Call the function being tested
-    const result = generateMetadataUrl(tokenAddress, tokenId);
+    const result = generateMetadataUrl(tokenId);
 
     // Verify the result
     expect(result).toBe(expectedUrl);
     expect(urlJoin).toHaveBeenCalledWith(
       expect.any(String),
-      tokenAddress,
       tokenId,
       'metadata'
     );
