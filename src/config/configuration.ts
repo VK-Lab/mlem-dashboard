ce594260a3b2c677c119a1b86493d96f9bc1b750
liam <hoailinh1210@gmail.com>
2023-09-11 12:19:42 +0700

    chore: update env metadata base url

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,8 @@
 export const Config = {
   chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
   apiBaseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://api.melem.io/v1',
-  metadataBaseUrl: 'https://api.melem.io/v1/nfts',
+  metadataBaseUrl:
+    process.env.NEXT_PUBLIC_METADATA_BASE_URL || 'https://api.melem.io/v1/nfts',
   nodeRPCUrl: '/rpc',
   casperDashUrl:
     process.env.NEXT_PUBLIC_CASPERDASH_API_URL || 'https://api.casperdash.io',
