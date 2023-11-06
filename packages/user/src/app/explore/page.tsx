b661ced7cc6dbbd206d25867e6d8534e99be6448
liam <hoailinh1210@gmail.com>
2023-11-06 20:05:24 +0700

    feat: mint nft

--- packages/user/src/app/explore/page.tsx
+++ packages/user/src/app/explore/page.tsx
@@ -1,5 +1,5 @@
 import { BaseContainer } from "@mlem-user/components/container";
-import { ListCampaigns } from "@mlem-user/modules/list-campaigns";
+import { ListCampaigns } from "@mlem-user/modules/campaign/list-campaigns";
 
 export default function Explore() {
   return (
