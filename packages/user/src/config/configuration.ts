ee1e11908191c4e5eed23ab81bbed04965e15e45
liam <hoailinh1210@gmail.com>
2023-11-16 10:03:54 +0700

    nit

--- packages/user/src/config/configuration.ts
+++ packages/user/src/config/configuration.ts
@@ -1,9 +1,7 @@
 export const Config = {
-  apiBaseUrl:
-    process.env.NEXT_PUBLIC_API_URL || "https://testnet-api.melem.io/v1",
-  sseBaseUrl:
-    process.env.NEXT_PUBLIC_SSE_URL || "https://testnet-api.melem.io/v1/sse",
-  networkName: process.env.NEXT_PUBLIC_CASPER_NETWORK || "casper-test",
+  apiBaseUrl: process.env.NEXT_PUBLIC_API_URL || "https://api.melem.io/v1",
+  sseBaseUrl: process.env.NEXT_PUBLIC_SSE_URL || "https://api.melem.io/v1/sse",
+  networkName: process.env.NEXT_PUBLIC_CASPER_NETWORK || "casper",
   metadataBaseUrl:
     process.env.NEXT_PUBLIC_METADATA_BASE_URL || "https://api.melem.io/v1/nfts",
   casperDashBaseUrl:
