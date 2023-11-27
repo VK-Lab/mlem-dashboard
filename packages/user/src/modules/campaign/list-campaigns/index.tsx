2caad99b91e3c53ac718149c9dabbf195e717956
liam <hoailinh1210@gmail.com>
2023-11-27 15:31:06 +0700

    refactor: remove count down

--- packages/user/src/modules/campaign/list-campaigns/index.tsx
+++ packages/user/src/modules/campaign/list-campaigns/index.tsx
@@ -11,7 +11,7 @@ export const ListCampaigns = () => {
   }
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="mt-4 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {data?.items?.map((item) => (
         <CampaignItem key={item.id} campaign={item} />
       ))}
