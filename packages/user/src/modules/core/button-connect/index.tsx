748f90af74c49d3e1ad397a1c11e733edd31b3b3
liam <hoailinh1210@gmail.com>
2023-11-28 16:38:15 +0700

    refactor: style for connect button

--- packages/user/src/modules/core/button-connect/index.tsx
+++ packages/user/src/modules/core/button-connect/index.tsx
@@ -5,6 +5,8 @@ import {
   CasperWalletConnector,
   useConnect,
 } from "@casperdash/usewallet";
+import CasperWalletLogo from "@mlem-user/assets/images/casper-wallet.png";
+import CasperDashLogo from "@mlem-user/assets/images/casperdash-logo.webp";
 import { Button } from "@mlem-user/components/ui/button";
 import {
   Dialog,
@@ -14,6 +16,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@mlem-user/components/ui/dialog";
+import Image from "next/image";
 
 type Props = {
   className?: string;
@@ -35,13 +38,35 @@ export const ButtonConnect = ({ className, buttonText = "Connect" }: Props) => {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle className="text-center mb-4">Connect Wallet</DialogTitle>
+          <DialogTitle className="text-center mb-4">
+            Connect Your Wallet
+          </DialogTitle>
           <DialogDescription className="flex justify-center flex-col align-center gap-4">
-            <Button className="w-full" onClick={connectCasperDashAsync}>
-              CasperDash
+            <Button className="w-full h-12" onClick={connectCasperDashAsync}>
+              <div className="flex items-center">
+                <div>
+                  <Image
+                    alt="casperdash logo"
+                    src={CasperDashLogo}
+                    width={30}
+                    height={30}
+                  />
+                </div>
+                <div className="text-md">CasperDash</div>
+              </div>
             </Button>
-            <Button className="w-full" onClick={connectCasperWalletAsync}>
-              Casper Wallet
+            <Button className="w-full h-12" onClick={connectCasperWalletAsync}>
+              <div className="flex items-center">
+                <div>
+                  <Image
+                    alt="casper wallet logo"
+                    src={CasperWalletLogo}
+                    width={18}
+                    height={18}
+                  />
+                </div>
+                <div className="text-md ml-2">Casper Wallet</div>
+              </div>
             </Button>
           </DialogDescription>
         </DialogHeader>
