7b5f2fadf11749fe98ab50f50925d42733acf54c
liam <hoailinh1210@gmail.com>
2023-12-01 15:22:10 +0700

    feat: check max owned tokens with completed transactions

---
+++ packages/user/src/modules/@core/nft-minter/hooks/use-get-completed-transaction.ts
@@ -0,0 +1,47 @@
+import { useMemo } from "react";
+
+import { useAccount } from "@casperdash/usewallet";
+import _get from "lodash-es/get";
+
+import { DeployStatusEnum } from "@mlem-user/enums";
+import { DeployActionsEnum } from "@mlem-user/enums/deployActions";
+import { DeployContextEnum } from "@mlem-user/enums/deployContext";
+import { Transaction } from "@mlem-user/types/transaction";
+
+import { useGetTransactions } from "../../../../hooks/transaction/use-get-transactions";
+
+type Params = {
+  contractPackageHash?: string;
+  action: DeployActionsEnum.MINT;
+};
+
+export const useGetCompletedTransactions = ({
+  contractPackageHash,
+  action,
+}: Params) => {
+  const { publicKey } = useAccount();
+  const { data, isLoading } = useGetTransactions(publicKey);
+
+  const foundTransactions = useMemo(() => {
+    if (!data || !contractPackageHash) {
+      return;
+    }
+
+    const transactions = data.filter((transaction: Transaction) => {
+      return (
+        transaction.action === action &&
+        transaction.context === DeployContextEnum.NFT &&
+        transaction.status === DeployStatusEnum.COMPLETED &&
+        _get(transaction, "additionalInfos.contractPackageHash") ===
+          contractPackageHash
+      );
+    });
+
+    return transactions;
+  }, [action, data, contractPackageHash]);
+
+  return {
+    transactions: foundTransactions,
+    isLoading,
+  };
+};
