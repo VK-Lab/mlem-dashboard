00c988c8bf1cf5a9b8512ee22bccb05b2d1d3f3d
liam <hoailinh1210@gmail.com>
2023-12-06 12:34:14 +0700

    feat: nft lucky button

--- packages/user/src/modules/nft/nft-detail/index.tsx
+++ packages/user/src/modules/nft/nft-detail/index.tsx
@@ -5,9 +5,11 @@ import { useAccount } from "@casperdash/usewallet";
 import { GradientAvatar } from "@mlem-user/components/avatar/gradient-avatar";
 import { NFTAsset } from "@mlem-user/components/nft-asset";
 import { SpinLoader } from "@mlem-user/components/ui/spin-loader";
+import { useCheckNFTIsLuckyBox } from "@mlem-user/services/app/nft/hooks/useCheckNFTIsLuckyBox";
 import { useGetNFT } from "@mlem-user/services/app/nft/hooks/useGetNFT";
 
 import { BenefitItem } from "./components/benefit-item";
+import { ButtonOpenBox } from "./components/button-open-box";
 import { TokenDetails } from "./components/token-details";
 
 type NFTDetailProps = {
@@ -22,6 +24,12 @@ export const NFTDetail = ({ contractPackageHash, tokenId }: NFTDetailProps) => {
     tokenId,
   });
   const { publicKey } = useAccount();
+  const { data: isLuckyBox } = useCheckNFTIsLuckyBox({
+    contractPackageHash,
+    tokenId,
+  });
+
+  console.log("isLuckyBox", isLuckyBox);
 
   if (isLoading) {
     return (
@@ -32,15 +40,19 @@ export const NFTDetail = ({ contractPackageHash, tokenId }: NFTDetailProps) => {
   }
 
   return (
-    <div className="flex gap-8">
-      <div className="flex flex-col flex-wrap">
-        <NFTAsset className="w-[400px] h-[400px]" url={data?.imageUrl} />
-        <div className="flex flex-col">
-          {/* <NFTAttributes /> */}
-          <TokenDetails nft={data} />
-        </div>
+    <div className="w-[800px] flex flex-row gap-8">
+      <div className="basis-[360px]">
+        <NFTAsset className="w-[360px] h-[360px]" url={data?.imageUrl} />
+        {isLuckyBox && (
+          <div className="mt-6">
+            <ButtonOpenBox
+              contractPackageHash={contractPackageHash}
+              tokenId={tokenId}
+            />
+          </div>
+        )}
       </div>
-      <div className="flex-auto">
+      <div className="flex-1">
         <div className="typo-h3">{data?.name}</div>
         <div className="mt-4 flex items-center gap-2">
           <GradientAvatar name={publicKey || ""} size={20} />
@@ -51,19 +63,26 @@ export const NFTDetail = ({ contractPackageHash, tokenId }: NFTDetailProps) => {
             {data?.description === "" ? "No description" : data?.description}
           </p>
         </div>
-        <div className="mt-6">
-          <p className="font-bold">Benefits</p>
-          <div className="flex flex-col mt-4 gap-6">
-            {data?.benefits.map((benefit) => (
-              <BenefitItem
-                key={`benefit-${benefit.id}`}
-                nftId={data.id}
-                benefit={benefit}
-                contractPackageHash={contractPackageHash}
-                tokenId={tokenId}
-              />
-            ))}
+
+        {data?.benefits && data?.benefits.length > 0 && (
+          <div className="mt-6">
+            <p className="font-bold">Benefits</p>
+            <div className="flex flex-col mt-4 gap-6">
+              {data?.benefits.map((benefit) => (
+                <BenefitItem
+                  key={`benefit-${benefit.id}`}
+                  nftId={data.id}
+                  benefit={benefit}
+                  contractPackageHash={contractPackageHash}
+                  tokenId={tokenId}
+                />
+              ))}
+            </div>
           </div>
+        )}
+        <div className="mt-6 flex flex-col">
+          {/* <NFTAttributes /> */}
+          <TokenDetails nft={data} />
         </div>
       </div>
     </div>
