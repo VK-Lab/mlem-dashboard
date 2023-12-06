f8246096c1fab69406984a92028f5fb26d493099
liam <hoailinh1210@gmail.com>
2023-12-07 01:04:34 +0700

    refactor: build static file for admin

--- packages/admin/src/pages/admin/benefit-categories/index.tsx
+++ packages/admin/src/pages/admin/benefit-categories/index.tsx
@@ -1,16 +1,7 @@
 import AdminBenefitCategory from '@mlem-admin/modules/AdminBenefitCategory';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const Index = () => {
   return <AdminBenefitCategory />;
-};
-
-export const getStaticProps = async ({ locale }: { locale: string }) => {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ['common'])),
-    },
-  };
 };
 
 export default Index;
--- packages/admin/src/pages/admin/claims/index.tsx
+++ packages/admin/src/pages/admin/claims/index.tsx
@@ -1,16 +1,7 @@
 import AdminHome from '@mlem-admin/modules/AdminHome';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const Index = () => {
   return <AdminHome />;
-};
-
-export const getStaticProps = async ({ locale }: { locale: string }) => {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ['common'])),
-    },
-  };
 };
 
 export default Index;
