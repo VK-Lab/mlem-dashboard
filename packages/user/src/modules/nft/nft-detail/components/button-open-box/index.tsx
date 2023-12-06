b322dac8570ffc7b950ca95d3d1268b74bf11144
liam <hoailinh1210@gmail.com>
2023-12-06 20:12:24 +0700

    feat: confetti when user open box

--- packages/user/src/modules/nft/nft-detail/components/button-open-box/index.tsx
+++ packages/user/src/modules/nft/nft-detail/components/button-open-box/index.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 import { useQueryClient } from "@tanstack/react-query";
+import Confetti from "react-confetti";
 
 import { Button } from "@mlem-user/components/ui/button";
+import { useToast } from "@mlem-user/components/ui/use-toast";
 import { QueryKeys } from "@mlem-user/enums/queryKeys";
+import useWindowSize from "@mlem-user/hooks/use-window-size";
 import { useOpenNFTLuckyBox } from "@mlem-user/services/app/nft/hooks/useOpenNFTLuckyBox";
 
 type Props = {
   contractPackageHash: string;
   tokenId: string;
+  isLuckyBox?: boolean;
 };
-export const ButtonOpenBox = ({ contractPackageHash, tokenId }: Props) => {
+export const ButtonOpenBox = ({
+  contractPackageHash,
+  tokenId,
+  isLuckyBox,
+}: Props) => {
+  const { toast } = useToast();
   const queryClient = useQueryClient();
+  const [isComplete, setIsComplete] = useState(false);
+  const { width, height } = useWindowSize();
+  const [isExploding, setIsExploding] = useState(false);
   const { mutate, isLoading } = useOpenNFTLuckyBox({
     onSuccess: async () => {
       await queryClient.invalidateQueries([
@@ -17,6 +31,15 @@ export const ButtonOpenBox = ({ contractPackageHash, tokenId }: Props) => {
         contractPackageHash,
         tokenId,
       ]);
+      toast({
+        title: "🎊 Congratulations",
+        description: "🌟 Bravo! Your NFT character has arrived! 🎉",
+      });
+      setIsExploding(true);
+
+      setTimeout(() => {
+        setIsComplete(true);
+      }, 8000);
     },
   });
 
@@ -28,8 +51,19 @@ export const ButtonOpenBox = ({ contractPackageHash, tokenId }: Props) => {
   };
 
   return (
-    <Button className="w-full" onClick={handleOnClaim} isLoading={isLoading}>
-      Reveal Your NFT
-    </Button>
+    <>
+      {isLuckyBox && (
+        <Button
+          className="w-full"
+          onClick={handleOnClaim}
+          isLoading={isLoading}
+        >
+          Reveal Your NFT
+        </Button>
+      )}
+      {isExploding && (
+        <Confetti width={width} height={height} recycle={!isComplete} />
+      )}
+    </>
   );
 };
--- packages/user/src/modules/nft/nft-detail/index.tsx
+++ packages/user/src/modules/nft/nft-detail/index.tsx
@@ -9,6 +9,7 @@ import { SpinLoader } from "@mlem-user/components/ui/spin-loader";
 import { Paths } from "@mlem-user/enums/paths";
 import { useCheckNFTIsLuckyBox } from "@mlem-user/services/app/nft/hooks/useCheckNFTIsLuckyBox";
 import { useGetNFT } from "@mlem-user/services/app/nft/hooks/useGetNFT";
+import { cn } from "@mlem-user/utils";
 
 import { BenefitItem } from "./components/benefit-item";
 import { ButtonOpenBox } from "./components/button-open-box";
@@ -51,14 +52,13 @@ export const NFTDetail = ({ contractPackageHash, tokenId }: NFTDetailProps) => {
     <div className="flex flex-row flex-wrap gap-8 justify-center">
       <div className="basis-[300px]">
         <NFTAsset className="w-[300px] h-[350px]" url={data?.imageUrl} />
-        {isLuckyBox && (
-          <div className="mt-6">
-            <ButtonOpenBox
-              contractPackageHash={contractPackageHash}
-              tokenId={tokenId}
-            />
-          </div>
-        )}
+        <div className={cn({ "mt-6": isLuckyBox })}>
+          <ButtonOpenBox
+            contractPackageHash={contractPackageHash}
+            tokenId={tokenId}
+            isLuckyBox={isLuckyBox}
+          />
+        </div>
       </div>
       <div className="flex-1 max-w-[300px]">
         <div className="typo-h3">{data?.name}</div>
