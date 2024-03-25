5669756d571fd6e5ed80d45f6eab7b8ca70cfd22
phatna03 <52144792+phatna03@users.noreply.github.com>
2024-03-25 09:13:26 +0700

    temporary open for tester off (#133)
    temporary open for tester off

--- packages/admin/src/utils/permission.ts
+++ packages/admin/src/utils/permission.ts
@@ -1,8 +1,5 @@
 import { RoleEnum, User } from '@mlem-admin/types/user';
 
 export const isAdmin = (user: User) => {
-  return (
-    (user.roles && user.roles.includes(RoleEnum.ADMIN)) ||
-    (user.roles && user.roles.includes(RoleEnum.USER))
-  );
+  return user.roles && user.roles.includes(RoleEnum.ADMIN);
 };
